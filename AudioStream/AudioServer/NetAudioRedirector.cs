e8d9e4510b1800a4b9f544fcf667781d607758e0
viyiviyi <viyiviyi@outlook.com>
2026-01-06 22:58:32 +0800

    解决延迟问题

--- AudioStream/AudioServer/NetAudioRedirector.cs
+++ AudioStream/AudioServer/NetAudioRedirector.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Hosting;
 namespace AudioStream.AudioServer
 {
     internal class NetAudioRedirector : IPlayerRedirector
@@ -14,11 +16,13 @@ namespace AudioStream.AudioServer
         private string _address;
         private WasapiOut wasapiOut;
         private Socket clientSocket;
-        private NetworkStream stream;
+        private Stream stream;
         private WaveFormat waveFormat;
         private NetworkStreamSource soundInSource;
+        private int maxDelaySize = 0;
         private Timer timer;
         private float _Volume = 1;
+        private bool isRun = false;
         public float Volume
         {
             get => _Volume; set
@@ -37,6 +41,7 @@ namespace AudioStream.AudioServer
         {
             _Volume = Volume;
             _address = address;
+            isRun = true;
             try
             {
                 // 连接到远程设备
@@ -47,7 +52,8 @@ namespace AudioStream.AudioServer
                 wasapiOut = new WasapiOut();
                 wasapiOut.Device = outputDevice;
                 wasapiOut.Latency = 5;
-                stream = new NetworkStream(clientSocket);
+                maxDelaySize = (waveFormat.BytesPerSecond / 100) * wasapiOut.Latency;
+                stream = new MemoryStream();
                 clientSocket.NoDelay = false;
                 clientSocket.ReceiveBufferSize = 32 * 1024;
                 clientSocket.SendBufferSize = 32 * 1024;
@@ -70,9 +76,69 @@ namespace AudioStream.AudioServer
                 }, null, 1000, 20 * 1000);
                 if (wasapiOut != null && wasapiOut.PlaybackState != PlaybackState.Playing)
                     wasapiOut.Play();
+                Task.Run(() =>
+                {
+                    using (var _stream = new NetworkStream(clientSocket))
+                    {
+                        var buff = new byte[1024 * 1024];
+                        while (isRun)
+                        {
+                            try
+                            {
+                                var len = _stream.Read(buff, 0, buff.Length);
+                                if (len <= 0)
+                                {
+                                    Thread.Sleep(1);
+                                    continue;
+                                }
+                                var data = new byte[Math.Min(len, maxDelaySize)];
+                                Buffer.BlockCopy(buff, len - data.Length, data, 0, data.Length);
+
+                                // 检查当前缓冲区大小
+                                long currentSize = stream.Length;
+                                long currentPosition = stream.Position;
+                                //Console.WriteLine($"{currentSize} {currentPosition} {len} {data.Length} {maxDelaySize}");
+
+                                // 如果有延迟
+                                if (currentSize > maxDelaySize)
+                                {
+                                    // 当有延迟时需要保留的最多数据量
+                                    long maxKeep = Math.Max(0, Math.Min(currentSize - currentPosition, maxDelaySize));
+                                    if (maxKeep > 0)
+                                    {
+                                        var cache = new byte[maxKeep];
+                                        stream.Position = stream.Length - maxKeep;
+                                        stream.Read(cache, 0, cache.Length);
+                                        stream.Position = 0;
+                                        stream.SetLength(0);
+                                        stream.Write(cache, 0, cache.Length);
+                                    }
+                                    stream.Position = 0;
+                                    stream.SetLength(maxKeep);
+                                }
+                                var position = stream.Position;
+                                stream.Position = stream.Length;
+                                stream.Write(data, 0, data.Length);
+                                stream.Position = position;
+                                if (wasapiOut != null && wasapiOut.PlaybackState != PlaybackState.Playing && stream.Length > maxDelaySize / wasapiOut.Latency)
+                                {
+                                    wasapiOut.Play();
+                                }
+
+                                // 更合理的延迟
+                                Thread.Sleep(1); // 增加延迟，减少CPU使用率
+                            }
+                            catch (Exception e)
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                });
             }
             catch (Exception ex)
             {
+                isRun = false;
                 Logger.Error($"连接出错 {ex.Message}\n{ex.StackTrace}", ex);
             }
         }
@@ -141,6 +207,7 @@ namespace AudioStream.AudioServer
 
         public void Dispose()
         {
+            isRun = false;
             timer?.Dispose();
             timer = null;
             try
@@ -169,10 +236,10 @@ namespace AudioStream.AudioServer
                 _waveFormat = waveFormat;
             }
             public WaveFormat WaveFormat => _waveFormat;
-            public long Length => -1; // Unknown length
-            public long Position { get; set; } // Not supported
+            public long Length => _stream.Length;
+            public long Position { get=> _stream.Position; set=> _stream.Position=value; }
 
-            public bool CanSeek => false;
+            public bool CanSeek => _stream.CanSeek;
 
             public int Read(byte[] buffer, int offset, int count)
             {
@@ -182,7 +249,7 @@ namespace AudioStream.AudioServer
                 }
                 catch (Exception ex)
                 {
-                    return 0; // Indicate end of stream or error
+                    return 0;
                 }
             }
             public void Dispose()
